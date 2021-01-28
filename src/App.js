a8b2bd4583688952362b9c419810e4898da840d1
fazufi <fazufi10@gmail.com>
2021-01-28 21:29:05 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Modal from "react-modal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    modalcuy: false,
+    nama: "FAZUFI",
+    isi: "",
+  };
+
+  bukamodal = () => {
+    this.setState({ modalcuy: true });
+  };
+
+  tutupmodal = () => {
+    this.setState({ modalcuy: false });
+  };
+
+  rubah = (e) => {
+    this.setState({ isi: e.target.value });
+  };
+
+  save = () => {
+    this.setState({ nama: this.state.isi });
+    this.setState({ modalcuy: false });
+  };
+
+  render() {
+    const { modalcuy, nama } = this.state;
+    return (
+      <div>
+        <h1 className="nama">{nama}</h1>
+        <button onClick={this.bukamodal}>KETUK UNTUK GANTI NAMA</button>
+
+        <Modal isOpen={modalcuy} onRequestClose={this.tutupmodal}>
+          <p className="gantinamamu">GANTI NAMAMU di kolom ini:</p>
+          <form>
+            <input
+              className="input"
+              type="text"
+              name="nama"
+              placeholder="ketik di sini"
+              onChange={this.rubah}
+            />
+          </form>
+          <button onClick={this.tutupmodal}>TUTUP</button>
+          <button onClick={this.save}>SAVE</button>
+        </Modal>
+      </div>
+    );
+  }
 }
-
-export default App;
